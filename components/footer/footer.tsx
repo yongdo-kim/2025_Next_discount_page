a7f386ac0990c93a9fd5352868d247731f400870
yongdo kim <nbmg1128@gmail.com>
2025-07-02 14:35:38 +0900

    feat mobile size udpate

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="mt-8 border-t border-neutral-200 py-2 dark:border-neutral-800">
+    <footer className="border-t border-neutral-200 py-2 dark:border-neutral-800">
       <div className="container mx-auto px-8 pt-4">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
           {/* 서비스 안내 */}
@@ -20,30 +20,6 @@ export default function Footer() {
                   서비스 소개
                 </Link>
               </li>
-              <li>
-                <Link
-                  href="/how-it-works"
-                  className="transition-colors hover:text-white"
-                >
-                  이용 방법
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/categories"
-                  className="transition-colors hover:text-white"
-                >
-                  카테고리
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/popular"
-                  className="transition-colors hover:text-white"
-                >
-                  인기 할인
-                </Link>
-              </li>
             </ul>
           </div>
 
@@ -56,31 +32,15 @@ export default function Footer() {
                   href="/faq"
                   className="transition-colors hover:text-white"
                 >
-                  자주 묻는 질문
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/contact"
-                  className="transition-colors hover:text-white"
-                >
                   문의하기
                 </Link>
               </li>
             </ul>
           </div>
-
-          {/* 연락처 정보 */}
-          <div>
-            <h4 className="font-semibold">문의하기</h4>
-            <address className="space-y-2 text-gray-400 not-italic">
-              <p>이메일: naristudio2023@gmail.com</p>
-            </address>
-          </div>
         </div>
 
         {/* 저작권 정보 */}
-        <div className="mb-4 border-gray-800 pt-4 text-center text-sm text-gray-500">
+        <div className="pt-8 pb-8 text-center text-sm text-neutral-300">
           <p> {currentYear} 할인탐정. All rights reserved.</p>
         </div>
       </div>
