54f51a50e2715ae73f3cb1f11733339f7ccc6382
yongdo kim <nbmg1128@gmail.com>
2025-07-03 16:52:21 +0900

    feat key added

--- features/category/presentation/components/category-random-area.tsx
+++ features/category/presentation/components/category-random-area.tsx
@@ -3,19 +3,18 @@
 import MainTitle from "@/components/main-title";
 import PostCardMiddle from "@/features/post/presentation/components/post-card-middle";
 import { useCategoryPostPreviews } from "@/features/post/presentation/hooks/use-posts";
-import { useWindowWidth } from "@/lib/hooks/use-window-width";
 
 export default function CategoryRandomArea() {
-  const { data: posts } = useCategoryPostPreviews()
+  const { data: posts } = useCategoryPostPreviews();
 
-  const width = useWindowWidth();
-  // sm, md는 4개, lg 이상은 5개, 모바일(640 미만)도 4개
-  let count = 4;
-  if (width >= 1024) count = 5;
+  // const width = useWindowWidth();
+  // // sm, md는 4개, lg 이상은 5개, 모바일(640 미만)도 4개
+  // let count = 4;
+  // if (width >= 1024) count = 5;
 
   const PostCardMiddleList = () => (
-    <ul className="grid grid-cols-1 gap-4 gap-y-4 px-4 pb-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
-      {posts?.slice(0, count).map((post) => {
+    <ul className="grid grid-cols-1 gap-4 gap-y-4 px-4 pb-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {posts?.slice(0, 4).map((post) => {
         if (!post) return null;
         return (
           <li key={post.id}>
