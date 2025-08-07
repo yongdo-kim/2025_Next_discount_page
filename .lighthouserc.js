02185a6e14ae7eb359035864a3e7d53c4db7502a
yongdo kim <nbmg1128@gmail.com>
2025-08-07 18:39:34 +0900

    feat light-house ci added

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -16,14 +16,22 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['warn', { minScore: 0.8 }],
-        'categories:accessibility': ['warn', { minScore: 0.8 }],
-        'categories:best-practices': ['warn', { minScore: 0.8 }],
-        'categories:seo': ['warn', { minScore: 0.8 }],
+        'categories:performance': ['error', { minScore: 0.8 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.8 }],
+        'categories:seo': ['error', { minScore: 0.8 }],
       },
     },
     upload: {
       target: 'temporary-public-storage',
+    },
+    server: {
+      port: 9001,
+      storage: {
+        storageMethod: 'sql',
+        sqlDialect: 'sqlite',
+        sqlDatabasePath: './lhci.db',
+      },
     },
   },
 };
