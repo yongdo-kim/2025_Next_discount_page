9b715a9459fe3d91579876bf82822b66e4900547
yongdo kim <nbmg1128@gmail.com>
2025-08-11 10:31:29 +0900

    feat themeprovider 복구

--- app/provider.tsx
+++ app/provider.tsx
@@ -3,6 +3,7 @@
 import { Toaster } from "@/components/shadcn/sonner";
 import { queryClient } from "@/lib/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
 import dynamic from "next/dynamic";
 const DynamicClarityScript = dynamic(() => import("@/components/analytics/ClarityScript"), {
   ssr: false,
@@ -21,9 +22,16 @@ const ReactQueryDevtools = process.env.NODE_ENV === "development"
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
         {children}
         <DynamicClarityScript />
         {ReactQueryDevtools && <ReactQueryDevtools />}
+      </ThemeProvider>
       <Toaster />
     </QueryClientProvider>
   );
