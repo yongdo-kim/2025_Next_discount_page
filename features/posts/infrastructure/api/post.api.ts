5488522b4c88d707efb33ed39ad2b90551b42dba
yongdo kim <nbmg1128@gmail.com>
2025-08-16 18:23:48 +0900

    chore 사용되지 않는 api 함수 정리

--- features/posts/infrastructure/api/post.api.ts
+++ features/posts/infrastructure/api/post.api.ts
@@ -1,18 +1,9 @@
 import { PostPreviewsResponse } from "@/features/posts/infrastructure/dto/responses/post-preview.res.dto";
-import { PostResponse, PostsResponse } from "@/features/posts/infrastructure/dto/responses/post.res.dto";
+import { PostResponse } from "@/features/posts/infrastructure/dto/responses/post.res.dto";
 import { apiClient } from "@/lib/api/client";
-import { PostCategory } from "@/features/posts/domain/types";
 import { PostPreviewsReqDto } from "@/features/posts/infrastructure/dto/requests/post-preview.req.dto";
 
 export const postApi = {
-  async getPosts({ category }: { category?: PostCategory }) {
-    const response = await apiClient.get<PostsResponse>({
-      url: "/posts",
-      query: category?.id ? `?categoryId=${category.id}` : "",
-    });
-    return response;
-  },
-
   async getPostPreviews({ req }: { req: PostPreviewsReqDto }) {
     const params = new URLSearchParams();
     if (req.categoryId) params.append("categoryId", req.categoryId.toString());
@@ -33,14 +24,6 @@ export const postApi = {
     return response;
   },
 
-  //카테고리 가져오기
-  async getPostCategories() {
-    const response =
-      await apiClient.get<PostPreviewsResponse>({
-        url: "/posts/categories",
-      });
-    return response.posts;
-  },
   //카테고리별 포스트 가져오기
   async getCategoryPostPreviews() {
     const response = await apiClient.get<PostPreviewsResponse>({
